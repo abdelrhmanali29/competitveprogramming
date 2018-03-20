166f952ae688120743f7ca33bbe16802289c54bd
abdelrhman_ali <36866988+abdelrhmanali29@users.noreply.github.com>
2018-03-20 21:09:55 +0200

    Create CF165-D2-B.cpp

---
+++ codeforces/CF165-D2-B.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<math.h>
+#include<iomanip>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<utility>
+#include<stack>
+#include<queue>
+#include<sstream>
+#include<bitset>
+#include<cstdio>
+#include<stdio.h>
+#include<cstring>
+#include <stdio.h>
+#include <math.h>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+typedef vector<ll>vl;
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define bit(n) (1<<(n))
+#define   fn           "\n"
+#define PI  3.14159265358979323846
+ll gcd(ll a, ll b){ if (b == 0){ return a; }return gcd(b, a % b); }
+void Abdo(){std::ios_base::sync_with_stdio(0);
+            cin.tie(NULL);
+            cout.tie(NULL);}
+const double E = 1e-12;
+typedef  long long ll;
+ll n,k;
+bool valid(ll mid){
+ll a=mid,r=k;
+while(mid/r>0){
+    a+=(mid/r);
+    r*=k;
+}
+return a>=n;
+}
+
+
+
+int main() {
+Abdo();
+cin>>n>>k;
+
+ll lo=1,hi=n;
+while(lo<hi){
+    ll mid=lo+(hi-lo)/2;
+    if(valid(mid))hi=mid;
+    else lo=mid+1;
+}
+cout<<lo<<fn;
+
+return 0;}
