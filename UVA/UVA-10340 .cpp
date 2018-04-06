f0932dfb63e2ef0f53bc65e4bb3169e2c735e242
abdelrhman_ali <36866988+abdelrhmanali29@users.noreply.github.com>
2018-04-06 20:41:02 +0200

    Create UVA-10340 .cpp

---
+++ UVA/UVA-10340 .cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<math.h>
+#include<iomanip>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<utility>
+#include<stack>
+#include<queue>
+#include<sstream>
+#include<bitset>
+#include<cstdio>
+#include<stdio.h>
+#include<cstring>
+#include <stdio.h>
+#include <math.h>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+typedef vector<ll>vl;
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define bit(n) (1<<(n))
+#define   fn           "\n"
+#define PI  3.14159265358979323846
+ll gcd(ll a, ll b){ if (b == 0){ return a; }return gcd(b, a % b); }
+void Abdo(){std::ios_base::sync_with_stdio(0);
+            cin.tie(NULL);
+            cout.tie(NULL);}
+
+
+int main() {
+Abdo();
+string s,t;
+//sequence subsequence
+while(cin>>s>>t){
+        char f=0;
+        int c=0;
+        for(int i=0;i<t.size();i++){
+            if(t[i]==s[f]){
+                f++;
+                c++;
+            }
+
+
+        }
+        if(c==s.size())cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+
+        }
+
+
+return 0;}
