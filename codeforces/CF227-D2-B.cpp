15c1b083369f9d18777c049f6f500fdc4ee77503
abdelrhman_ali <36866988+abdelrhmanali29@users.noreply.github.com>
2018-11-20 13:55:20 +0200

    Create CF227-D2-B.cpp

---
+++ codeforces/CF227-D2-B.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<math.h>
+#include<iomanip>
+#include<string>
+#include<vector>
+#include<set>
+#include<map>
+#include<utility>
+#include<stack>
+#include<queue>
+#include<sstream>
+#include<bitset>
+#include<cstdio>
+#include<stdio.h>
+#include<cstring>
+#include<stdio.h>
+#include<math.h>
+#include<cmath>
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll>vl;
+typedef long double ld;
+
+#define cn(n)  scanf("%d", &n)
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define F(i, a, b) for (int i=a; i<=b; i++)
+#define rep(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define bit(n) (1<<(n))
+#define   fn           "\n"
+//#define PI  3.14159265358979323846
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+void Abdo()
+{
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+//3.14159265358979323846264338327950288419716939937510582097494459;
+int dx[]= {0, -1, -1, -1, 0, 1, 1,  1};
+int dy[]= {-1, -1,  0,  1, 1, 1, 0, -1};
+const ld EPS = 1e-9, PI = 3.1415926535897932384626433832795;
+
+const long long N = 1e5 + 5, M = 1e5 +5, OO = 0x3f3f3f3f;
+
+
+int main()
+{
+	ll n, m, a, q , idx[N] , ans1 = 0 , ans2 = 0;
+	cin>>n;
+	rep(i ,n){
+		cin>>a;
+		idx[a] = i;
+	}
+	n--;
+	cin>>m;
+	while(m--){
+		cin>>q;
+		ans1 += idx[q]+1;
+		ans2 += n-idx[q]+1;
+	}
+	cout<<ans1<<' '<<ans2<<endl;
+
+return 0;
+}
