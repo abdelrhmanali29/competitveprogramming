6cfe9ef93778aa111ea507a6ae8acb8121a658a3
abdelrhman_ali <36866988+abdelrhmanali29@users.noreply.github.com>
2019-02-02 10:21:24 +0200

    Update UVA-11413 .cpp

--- UVA/UVA-11413 .cpp
+++ UVA/UVA-11413 .cpp
@@ -7,76 +7,96 @@
 #include<vector>
 #include<set>
 #include<map>
-#include<utility>
 #include<stack>
 #include<queue>
 #include<sstream>
 #include<bitset>
-#include<cstdio>
-#include<stdio.h>
 #include<cstring>
-#include <stdio.h>
-#include <math.h>
-#include <cmath>
+#include<stdio.h>
+#include<math.h>
+#include<cmath>
+
 using namespace std;
-typedef long long ll;
+typedef long long int ll;
 typedef vector<ll>vl;
-#define FileIn(file) freopen(file".inp", "r", stdin)
+typedef long double ld;
+#define boom main
+#define cn(n)  scanf("%d", &n)
+#define cnl(n)  scanf("%lld", &n)
+#define FileIn(file) freopen(file".in", "r", stdin)
 #define FileOut(file) freopen(file".out", "w", stdout)
-#define FOR(i, a, b) for (int i=a; i<=b; i++)
-#define REP(i, n) for (int i=0; i<n; i++)
+#define F(i, a, b) for(int i=a; i<=b; i++)
+#define rep(i, n) for(int i=0; i<n; i++)
 #define Fill(ar, val) memset(ar, val, sizeof(ar))
 #define bit(n) (1<<(n))
 #define   fn           "\n"
-#define PI  3.14159265358979323846
-ll gcd(ll a, ll b){ if (b == 0){ return a; }return gcd(b, a % b); }
-void Abdo(){std::ios_base::sync_with_stdio(0);
-            cin.tie(NULL);
-            cout.tie(NULL);}
-const double E = 1e-12;
-typedef  long long ll;
-void file(){freopen("in.txt", "r", stdin);
-freopen("out.txt", "w", stdout);}
-
-vl vec;
-bool check(ll mid, int m)
+//#define PI  3.14159265358979323846
+void file()
 {
-	ll ans = 1, ss = 0;
-	for (int i = 0; i < vec.size(); i++)
-	{
-		if (ss + vec[i] <= mid)
-			ss = ss + vec[i];
-		else if (ans + 1 <= m)
-			ss = 0, ans++, i--;
-		else
-			return false;
-	}
-	if (ans <= m)
-		return true;
-	return false;
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
 }
-int main() {
-Abdo();
+void Abdo()
+{
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+ll countBits(ll number)
+{
+    return (ll)log2(number)+1;
+}
+//3.14159265358979323846264338327950288419716939937510582097494459;
+int dx[]= {0, -1, -1, -1, 0, 1, 1,  1};
+int dy[]= {-1, -1,  0,  1, 1, 1, 0, -1};
+const ld EPS = 1e-9, PI = 3.14159;
+const long long N = 1e4 + 5, M = 1e5 +5, OO = 0x3f3f3f3f, MOD = 1e9+7;
+ll arr[N];
 int n, m;
-	ll lo, hi, mid, z;
-	while (cin >> n >> m)
-	{
-		vec.resize(n);
-		for (int i = 0; i < n; i++)
-			cin >> vec[i];
-		lo = 1, hi = 1e10;
-		while (lo <= hi)
-		{
-			mid = (lo + hi) / 2;
-			if (check(mid, m))
-			{
-				hi = mid - 1;
-				z = mid;
-			}
-			else
-				lo = mid + 1;
-		}
-		cout << z << "\n";
-	}
+bool valid(ll mid)
+{
+    int sum = 0, cnt = 0;
+    rep(i, n)
+    {
+    	if(sum + arr[i] <= mid)sum += arr[i];
+    	else{
+		sum = arr[i];
+		cnt++;
+    	}
+    }
+    	if(sum)cnt++;
+	return cnt <= m;
+}
 
-return 0;}
+
+int boom(void)
+{
+    while(scanf("%d %d", &n,&m) != EOF)
+    {
+        ll mx = 0;
+        rep(i, n)
+        {
+            cin>>arr[i];
+            mx = max(mx, arr[i]);
+        }
+        ll s = mx, e = 1e10, ans = 0;
+        while(s <= e)
+        {
+            ll mid = s+e>>1;
+            if(valid(mid))
+            {
+                ans = mid;
+                e = mid -1;
+            }
+            else s = mid + 1 ;
+        }
+
+        cout<<ans<<fn;
+
+    }
+
+
+}
+
+
+
